565ba14cbc23a1a4be95d6333d9abf23e449f020
DreamerAkhilesh <imakhileshpratap@gmail.com>
2025-04-17 22:26:48 +0530

    Changed contant.js

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,3 +1,4 @@
-export const USER_API_END_POINT = 'http://localhost:3000/api/v1/user';
-export const ADMIN_API_END_POINT = 'http://localhost:3000/api/v1/admin';
-export const GOOGLE_MAPS_API_KEY = 'YOUR_GOOGLE_MAPS_API_KEY';
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
+export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
+export const ADMIN_API_END_POINT = `${BASE_URL}/api/v1/admin`;
